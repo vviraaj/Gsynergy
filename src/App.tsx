74882fcd5c65b33f1eb0d48edaa4c28e691bdcdb
rahulPandey1995 <rahul.p@antino.com>
2025-03-09 12:24:18 +0530

    lazy load added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import StoresPage from "./pages/StoresPage";
-import SKUsPage from "./pages/SkUS/SKUsPage";
-import PlanningPage from "./pages/PlanningPage";
-import ChartsPage from "./pages/ChartsPage";
 import Login from "./components/login";
 import PrivateRoute from "./components/Routegurad";
+
+// Lazy-loaded components
+const StoresPage = lazy(() => import("./pages/StoresPage"));
+const SKUsPage = lazy(() => import("./pages/SkUS/SKUsPage"));
+const PlanningPage = lazy(() => import("./pages/PlanningPage"));
+const ChartsPage = lazy(() => import("./pages/ChartsPage"));
 
 function App() {
   return (
@@ -24,10 +26,38 @@ function App() {
             </PrivateRoute>
           }
         >
-          <Route path="stores" element={<StoresPage />} />
-          <Route path="skus" element={<SKUsPage />} />
-          <Route path="planning" element={<PlanningPage />} />
-          <Route path="charts" element={<ChartsPage />} />
+          <Route
+            path="stores"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <StoresPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="skus"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <SKUsPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="planning"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <PlanningPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="charts"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ChartsPage />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
